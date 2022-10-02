c1c6ef588a4ed7bcce4e4e945593763bcfdeb81f
Aman Srivastava <89743268+amansrv@users.noreply.github.com>
2022-10-02 16:40:44 +0530

    Create Single LL.cpp

---
+++ Linked List/Single LL.cpp
@@ -0,0 +1,65 @@
+// INSERTION OF NODE AT HEAD NODE IN LL: INSERTION OF HEAD
+
+/*
+STEPS:
+Step-1: Use InsertAtHead() for add node
+Step-2: Remove Null pointer tempnode
+Step-3: Shift haed to Head node
+
+*/
+
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    // constructor
+    Node(int data)
+    {
+        this->data = data;
+        this->next = 0;
+    }
+};
+
+void insertAtHead(Node *&head, int d)
+{
+    // New node create
+    Node *temp = new Node(d);
+    temp->next = head;
+    head = temp;
+}
+
+void print(Node *&head)
+{
+
+    Node *temp = head;
+    while (temp != 0)
+    {
+
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    // created the new node
+    Node *node1 = new Node(10);
+
+    // Head Node
+    Node *head = node1;
+    print(head);
+
+    insertAtHead(head, 12);
+    print(head);
+
+    insertAtHead(head, 15);
+    print(head);
+
+    return 0;
+}
