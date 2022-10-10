12fd227f257c7f33824caf56c0b521f093b86fad
Aman Srivastava <89743268+amansrv@users.noreply.github.com>
2022-10-10 19:33:56 +0530

    Create init.cpp

---
+++ Arrays/init.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Declaration of array
+    int a[5];
+
+    cout << "value at index 0 =" << a[0] << endl;
+    cout << "value at index 1 =" << a[1] << endl;
+    cout << "value at index 2 =" << a[2] << endl;
+    cout << "value at index 3 =" << a[3] << endl;
+    cout << "value at index 4 =" << a[4] << endl;
+    cout << "value at index 5 =" << a[5] << endl;
+
+    cout << endl;
+  
+    // init of array
+    int b[6] = {1, 2, 3, 4, 5, 6};
+    cout << "value at index 0 =" << b[0] << endl;
+    cout << "value at index 1 =" << b[1] << endl;
+    cout << "value at index 2 =" << b[2] << endl;
+    cout << "value at index 3 =" << b[3] << endl;
+    cout << "value at index 4 =" << b[4] << endl;
+    cout << "value at index 5 =" << b[5] << endl;
+  
+    return 0;
+}
