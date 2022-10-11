4ee8f9dad230a656d5d8a3c1a057c245a8588b68
Aman Srivastava <89743268+amansrv@users.noreply.github.com>
2022-10-11 23:02:40 +0530

    Create PrintIndex.cpp

---
+++ Arrays/PrintIndex.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // print all value at index of an array
+    int n1[10] = {9, 8};
+    // int n = 10;
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "intex: " << n1[i] << endl;
+    }
+
+    return 0;
+}
