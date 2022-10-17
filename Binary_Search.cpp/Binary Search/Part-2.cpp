37f595354f6b59d745df1aaced0abcd3b0e834b9
Aman Srivastava <89743268+amansrv@users.noreply.github.com>
2022-10-17 19:54:08 +0530

    Add files via upload

---
+++ Binary_Search.cpp/Binary Search/Part-2.cpp
@@ -0,0 +1,45 @@
+// Binary Search in C++
+
+#include <iostream>
+#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+bool BinarySearch(int arr[], int size, int key)
+{
+    // init  S and E with find out the mid value
+    int start = 0;
+    int end = size - 1;
+    int mid = start + end / 2;
+
+    // comapre the mid with array size
+    while (start <= end)
+    {
+        if (arr[mid] == key)
+        {
+            return mid;
+        }
+    }
+
+    if (key > arr[mid])
+    {
+        // Left part
+        start = mid + 1;
+    }
+    else
+    {
+        // right part
+        end = mid - 1;
+    }
+}
+
+int main()
+{
+
+    int even[6] = {4, 6, 8, 10, 15, 20};
+
+    int evenindex = BinarySearch(even, 6, 12);
+    cout << "index of 12 = " << evenindex << endl;
+
+    return 0;
+}
---
+++ Binary_Search.cpp/Binary Search/PrintArrayUserIP.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[100];
+    cout << "the array size is= " << endl;
+    cin >>
+
+        return 0;
+}
